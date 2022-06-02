fb1ce1d6759170fdb35c26096c9531f3b53a2835
Danya Provilskyi <65830545+Danila-cloud@users.noreply.github.com>
2022-06-02 14:22:29 +0200

    add score and lives canva

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class CoinPickup : MonoBehaviour
 {
     [SerializeField] AudioClip CoinPickupSFX;
+    int pointsToAdd = 30;
+    bool wasCollected = false;
     void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.tag == "Player")
+        if(other.tag == "Player" && !wasCollected)
         {
+            wasCollected = true;
+            FindObjectOfType<GameSession>().AddScore(pointsToAdd);
             AudioSource.PlayClipAtPoint(CoinPickupSFX, Camera.main.transform.position);
+            gameObject.SetActive(false);
             Destroy(gameObject);
         }
     }
--- Assets/Scripts/GameSession.cs
+++ Assets/Scripts/GameSession.cs
@@ -2,10 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameSession : MonoBehaviour
 {
     [SerializeField] int PlayerLives = 3;
+
+    [SerializeField] TextMeshProUGUI livesText;
+    [SerializeField] TextMeshProUGUI scoreText;
+
+    int score = 0;
 
     void Awake()
     {
@@ -19,8 +25,16 @@ public class GameSession : MonoBehaviour
             DontDestroyOnLoad(gameObject);
         }
     }
+    public void AddScore(int pointsToAdd)
+    {
+        score += pointsToAdd;
+    }
 
-
+    void Update()
+    {
+        livesText.text = PlayerLives.ToString();
+        scoreText.text = score.ToString();
+    }
     public void DEATH()
     {
         if(PlayerLives > 1)
@@ -37,6 +51,7 @@ public class GameSession : MonoBehaviour
         PlayerLives--;
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex);
+        livesText.text = PlayerLives.ToString();
     }
     void ResetSession()
     {
